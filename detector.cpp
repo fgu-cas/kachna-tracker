b2874f6d35d8c3d15cfac3d86def4a04b908ad1e
Tomáš Mládek <tomas0mladek@gmail.com>
2016-01-21 12:56:30 +0100

    prevent crashes when mask extends beyond image

--- detector.cpp
+++ detector.cpp
@@ -11,8 +11,8 @@ Detector::Detector(const QMap<QString, QVariant> &settings, int h, int w){
     mask.setTo(Scalar(0));
     circle(mask, Point2f(x, y), r, Scalar(255), -1);
 
-    maskRect.x = x-r;
-    maskRect.y = y-r;
+    maskRect.x = (r<x ? x-r : 0);
+    maskRect.y = (r<y ? y-r : 0);
     maskRect.width = r*2;
     maskRect.height = r*2;
 }
